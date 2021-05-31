b4ad0ff9428a72d781e6873486747b232e69dc38
JossepP99 <jossepparedes1@gmail.com>
2021-05-31 17:06:49 -0400

    textarea added to a modal 0.0003

--- resources/views/Evaluacion/Fortaleza.blade.php
+++ resources/views/Evaluacion/Fortaleza.blade.php
@@ -7,27 +7,56 @@
 @section('contenido')
 
 <div class="col-md-12">
-
     <div class="card">
         <div class="card-header">
             <div class="barra">
-            <h1>Fortalezas</h1>
+            <h1>Fortalezas</h1>    
+<!-- Button trigger modal -->
             <a href="#" data-toggle="modal" data-target="#exampleModalCenter"> <i class="fas fa-plus"> Añadir nuevo</i></a>
-
+            
             </div>
         </div>
+
+<!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                    
+                            <form>
+                                <div class="campo">
+                                  <textarea class="form-control1" id="message-text" autofocus></textarea>
+                                </div>
+                            </form>
+                        
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
         <div class="card-body">
             <div class="table-responsive">
                 <table id="basic-datatables" class="display table table-striped table-hover table-boder-radius">
                     <thead>
+                        {{-- insertion of items --}}
                         <tr>
                             <th>Nº item</th>
                             <th>Descripcion</th>
                             <th>Acciones</th>
                         </tr>
                     </thead>
+                    {{-- foot table --}}
                     <tfoot>
                         <tr>
                             <th>Nº item</th>
@@ -36,6 +65,7 @@
                         </tr>
                     </tfoot>
 
+                    {{--database data subtraction --}}
                     <tbody>
                         <tr>
                             <td>Tiger Nixon</td>
@@ -49,8 +79,6 @@
         </div>
     </div>
 </div>
-
-
 
 
 @endsection
